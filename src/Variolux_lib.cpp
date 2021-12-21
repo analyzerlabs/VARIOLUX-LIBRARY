02e5d60515f890a5a5bfd8a716004c177c5cea33
MiguelQuispe <fisicomiguel@gmail.com>
2021-12-21 12:36:28 -0500

    firmware firmware-ECU-VL-002.bin

--- src/Variolux_lib.cpp
+++ src/Variolux_lib.cpp
@@ -28,6 +28,29 @@
 
 class variolux{
     private:
+        const char* ssid = "Smart Solutions Labs";
+        const char* password = "20120415H";
+
+        char* test_root_ca= \
+            "-----BEGIN CERTIFICATE-----\n"  
+        "MIIDxTCCAq2gAwIBAgIQAqxcJmoLQJuPC3nyrkYldzANBgkqhkiG9w0BAQUFADBsMQswCQYDVQQG\n"  
+        "EwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3d3cuZGlnaWNlcnQuY29tMSsw\n"  
+        "KQYDVQQDEyJEaWdpQ2VydCBIaWdoIEFzc3VyYW5jZSBFViBSb290IENBMB4XDTA2MTExMDAwMDAw\n"  
+        "MFoXDTMxMTExMDAwMDAwMFowbDELMAkGA1UEBhMCVVMxFTATBgNVBAoTDERpZ2lDZXJ0IEluYzEZ\n"  
+        "MBcGA1UECxMQd3d3LmRpZ2ljZXJ0LmNvbTErMCkGA1UEAxMiRGlnaUNlcnQgSGlnaCBBc3N1cmFu\n"  
+        "Y2UgRVYgUm9vdCBDQTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMbM5XPm+9S75S0t\n"  
+        "Mqbf5YE/yc0lSbZxKsPVlDRnogocsF9ppkCxxLeyj9CYpKlBWTrT3JTWPNt0OKRKzE0lgvdKpVMS\n"  
+        "OO7zSW1xkX5jtqumX8OkhPhPYlG++MXs2ziS4wblCJEMxChBVfvLWokVfnHoNb9Ncgk9vjo4UFt3\n"  
+        "MRuNs8ckRZqnrG0AFFoEt7oT61EKmEFBIk5lYYeBQVCmeVyJ3hlKV9Uu5l0cUyx+mM0aBhakaHPQ\n"  
+        "NAQTXKFx01p8VdteZOE3hzBWBOURtCmAEvF5OYiiAhF8J2a3iLd48soKqDirCmTCv2ZdlYTBoSUe\n"  
+        "h10aUAsgEsxBu24LUTi4S8sCAwEAAaNjMGEwDgYDVR0PAQH/BAQDAgGGMA8GA1UdEwEB/wQFMAMB\n"  
+        "Af8wHQYDVR0OBBYEFLE+w2kD+L9HAdSYJhoIAu9jZCvDMB8GA1UdIwQYMBaAFLE+w2kD+L9HAdSY\n"  
+        "JhoIAu9jZCvDMA0GCSqGSIb3DQEBBQUAA4IBAQAcGgaX3NecnzyIZgYIVyHbIUf4KmeqvxgydkAQ\n"  
+        "V8GK83rZEWWONfqe/EW1ntlMMUu4kehDLI6zeM7b41N5cdblIZQB2lWHmiRk9opmzN6cN82oNLFp\n"  
+        "myPInngiK3BD41VHMWEZ71jFhS9OMPagMRYjyOfiZRYzy78aG6A9+MpeizGLYAiJLQwGXFK3xPkK\n"  
+        "mNEVX58Svnw2Yzi9RKR/5CYrCsSXaQ3pjOLAEFe4yHYSkVXySGnYvCoCWw9E1CAx2/S6cCZdkGCe\n"  
+        "vEsXCS+0yx5DaMkHJ8HSXPfqIbloEpw8nL+e/IBcm2PN7EeqJSdnoDfzAIJ9VNep+OkuE6N36B9K\n" 
+        "-----END CERTIFICATE-----\n" ;
         int a;
         int NUM_colors=134;
         int tablaRGB[134][3];
@@ -74,12 +97,15 @@ class variolux{
         bool flag_change_color=false;
         int encoder2Timer = 0;
         int LAST_VP_ENCODER_SCREEN = 0;
+        WiFiClientSecure clientForOta;
+        
     public:  
-        variolux(String tipo){ 
+        variolux(String tipo){      
             Nombre = tipo;
             if(Nombre == "Equipo002"){
                 EEPROM.begin(500);
                 Serial2.begin(9600);
+                firmmwareUpdaterSetup();
                 //bleInit(Nombre);
                 #if _DEBUG_
                     Serial.begin(9600);
@@ -131,7 +157,34 @@ class variolux{
                 runtime=millis();
             }
         }
-        
+        void firmmwareUpdaterSetup(){
+            
+                WiFi.mode(WIFI_STA);
+                WiFi.begin(ssid, password);
+                byte retries = 0;
+                while (WiFi.status() != WL_CONNECTED) {
+                    Serial.print(".");
+                    // wait 1 second for re-trying
+                    delay(1000);
+                    retries++;
+                    if((retries % 5) == 4) WiFi.begin(ssid, password); //solves AUTH_FAIL bug on some routers
+                }
+                
+        }
+        void firmwareUpdate(){
+            secureEsp32FOTA fota ("ECU-VL-002", 1);
+            fota._host="raw.githubusercontent.com"; //e.g. example.com
+            fota._descriptionOfFirmwareURL="https://github.com/analyzerlabs/VARIOLUX-LIBRARY/main/src/firmware/firmware.json"; //e.g. /my-fw-versions/firmware.json
+            fota._certificate=test_root_ca;
+            fota.clientForOta=clientForOta;
+
+            bool shouldExecuteFirmwareUpdate=fota.execHTTPSCheck();
+            if(shouldExecuteFirmwareUpdate)
+            {
+                Serial.println("Firmware update available!");
+                fota.executeOTA();
+            }
+        }
 
         void setPins(){
             for(int i=0;i<VarioplusDimeableSteps;i++){
